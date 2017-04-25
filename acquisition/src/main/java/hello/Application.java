03930105835cb14638418b86c5db7a8b95eae952
Yuting Zhou <zyt1231@gmail.com>
2017-04-24 20:33:40 -0400

    hellooo spring

---
+++ acquisition/src/main/java/hello/Application.java
@@ -0,0 +1,19 @@
+package hello;
+
+/**
+ * Created by Ting on 4/24/17.
+ */
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestTemplate;
+public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String args[]) {
+        RestTemplate restTemplate = new RestTemplate();
+        Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        log.info(quote.toString());
+    }
+
+}
